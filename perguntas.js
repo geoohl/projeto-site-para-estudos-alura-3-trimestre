f2c644251fa817611af92c856ef81a1783f20794
geoohl <163031892+geoohl@users.noreply.github.com>
2024-11-20 23:49:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Irlanda?',
+    'A capital da Irlanda é Dublin'
+)
+
+criaCartao(
+    'Programação',
+    'O que são estruturas de decisão compostas?',
+    'Como o if-else e elif. Essas estruturas permitem tomar decisões no seu código, executando diferentes blocos de código dependendo de uma condição.'
+)
+
+criaCartao(
+    'Linguagem Irlandês',
+    'Como se diz Boa Tarde em Irlandês?',
+    'Boa tarde" se diz "Tráthnóna maith" (pronuncia-se "trah-NOH-nah mah").'
+)
